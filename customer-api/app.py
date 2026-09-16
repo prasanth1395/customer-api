bfbf02057ddf7d51488aa9d5e873c0982684f298
prasanth1395 <46778141+prasanth1395@users.noreply.github.com>
2026-09-16 07:38:01 +0530

    Update app.py

--- customer-api/app.py
+++ customer-api/app.py
@@ -13,4 +13,4 @@ def health():
 #if __name__ == "__main__":
 #    app.run(host="0.0.0.0", port=8080)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    raise Exception("Intentional failure for SRE troubleshooting lab")
